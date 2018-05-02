3d5fc52b05753095a564c9360620c50f64dc50cd
胡义晋 <jingdiqkl123@qq.com>
2018-05-02 10:54:34 +0800

    签到

--- client/common/constant_url.js
+++ client/common/constant_url.js
@@ -5,5 +5,5 @@ module.exports = {
   UserInfo: host + '/user/info',//获取用户信息
   
   Check: host + '/check',//签到
-  CheckHistory: host + '/history/history'//获取某个月的签到历史
+  CheckHistory: host + '/check/history'//获取某个月的签到历史
 }
